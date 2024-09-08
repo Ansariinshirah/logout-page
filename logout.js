0eef3a7dc5de9f07990919e2b51482844b19ea2c
Ansariinshirah <inshiraansari786@gmail.com>
2024-09-08 22:34:41 +0530

    Add files via upload

---
+++ logout.js
@@ -0,0 +1,9 @@
+function handleCancel() {
+    // Handle cancel logic here (e.g., close the modal or redirect)
+    console.log("Cancel clicked");
+  }
+  
+  function handleLogout() {
+    // Handle logout logic here (e.g., make API call, update state, redirect)
+    console.log("Logout clicked");
+  }
